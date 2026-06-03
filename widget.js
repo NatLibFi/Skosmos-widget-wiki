1f4866520d8281da477760d61b68c2d3f9e3ad27
Vainonen <8686308+Vainonen@users.noreply.github.com>
2026-06-03 15:04:05 +0300

    Added Wikidata API request and fixed code and CSS styles

--- widget.js
+++ widget.js
@@ -7,43 +7,43 @@ const WIKI = {
       data () {
         return {
           succeeded: WIKI.succeeded,
-          //wikipediaLang: WIKI.wikiLang,
-          wikipediaCaption: WIKI.getTranslation("wikipediaCaption"),
-          wikipediaTermsAndConditions: WIKI.getTranslation("wikipediaTerms"),
-          wikipediaCredit: WIKI.getTranslation("wikipediaCredit"),
+          message: WIKI.message,
+          wikipediaLang: WIKI.wikiLang,
+          wikipediaCaption: WIKI.getTranslation('wikipediaCaption'),
+          wikipediaTermsAndConditions: WIKI.getTranslation('wikipediaTerms'),
+          wikipediaCredit: WIKI.getTranslation('wikipediaCredit'),
           wikipediaURL: WIKI.wikipediaURL,
-          restURL: WIKI.restURL,
           wikipediaHTML: WIKI.wikipediaHTML,
           mappings: {},
           loading: true
         }
       },
-      template: `<div class="concept-widget panel-group" id="wikiAccordion" role="tablist" aria-multiselectable="true">
+      template: `<div id="wiki-widget panel-group" role="tablist" aria-multiselectable="true">
                   <div class="panel panel-default">
                     <div class="panel-heading" role="tab" id="headingWiki">
-                      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWiki" aria-expanded="true" aria-controls="collapseWiki">
+                      <button
+                       class="accordion-button accordion"
+                       type="button"
+                       data-bs-toggle="collapse"
+                       data-bs-target="#collapseWiki"
+                       aria-expanded="true"
+                       aria-controls="collapseWiki"
+                      >
                         {{wikipediaCaption}}
                       </button>
-                      <!--
-                      <div v-if="wikipediaLang.diff">
-                        {{wikipediaLang.value}}
-                      </div>
-                      <div v-else>
-                        {{wikipediaCaption}}
-                      </div>
-                      -->
                     </div>
-                    <div id="collapseWiki" class="panel-collapse collapse show sidebar-grey" role="tabpanel" aria-labelledby="headingWiki">
+                    <div id="collapseWiki" class="panel-collapse collapse show" role="tabpanel" aria-labelledby="headingWiki">
                       <div class="panel-body">
-                        <div v-html="wikipediaHTML" id="wiki" class="panel mw-parser-output" role="tabpanel" aria-labelledby="headingWikiWidget">
+                        <div id="wiki" class="panel mw-parser-output" role="tabpanel" aria-labelledby="headingWikiWidget">
+                          <div v-if="succeeded" v-html="wikipediaHTML"></div>
+                          <div class="wiki-missing" v-else>{{message}}</div>
                         </div>
-                        <!--
-                        <iframe :src="restURL"></iframe>
-                        -->
                       </div>
-                      <div class="wikipedia-disclaimer versal">
+                      <div v-if="succeeded" class="wikipedia-disclaimer versal">
                         <div v-html="wikipediaTermsAndConditions" class="wikipedia-terms"></div>
-                        <div class="wikipedia-credit">{{wikipediaCredit}}<a href="{{wikipediaURL}}" target="_blank">{{wikipediaURL}}</a></div>
+                        <div class="wikipedia-credit">{{wikipediaCredit}}
+                          <a href="{{wikipediaURL}}" target="_blank">{{wikipediaURL}}</a>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -51,131 +51,156 @@ const WIKI = {
               `
     })
   },
-  succeeded: false,
+  succeeded: true,
   wikiLang: null,
   wikipediaURL: null,
-  restURL: null,
   wikipediaHTML: null,
-  address: "",
+  address: '',
   getTranslation: function (key) {
-    const getLang = window.SKOSMOS.lang;
-    if (getLang !== "fi" && getLang !== "sv") {
-      getLang = "en";
+    let getLang = window.SKOSMOS.lang
+    if (getLang !== 'fi' && getLang !== 'sv') {
+      getLang = 'en'
     }
-    if (key === "404") {
+    if (key === '404') {
       return {
-        "fi": "Ei wikipedia-sivua sanaston tukemalla kielellä.",
-        "sv": "Inte något wikipedia-sidan på vokabulär språk.",
-        "en": "No wikipedia article on any vocabulary language."
-      }[getLang];
+        fi: 'Ei wikipedia-sivua sanaston tukemilla kielillä.',
+        sv: 'Inte något wikipedia-sidan på vokabulär språk.',
+        en: 'No wikipedia article on any vocabulary language.'
+      }[getLang]
     }
-    if (key === "error") {
+    if (key === 'error') {
       return {
-        "fi": "Wikipedia-sivun lataamisessa tapahtui virhe.",
-        "sv": "Wikipedia-sidan kan inte laddas.",
-        "en": "Could not load wikipedia page."
-      }[getLang];
-    }
-    else if (key === "wikipediaCaption") {
+        fi: 'Wikipedia-sivun lataamisessa tapahtui virhe.',
+        sv: 'Wikipedia-sidan kan inte laddas.',
+        en: 'Could not load wikipedia page.'
+      }[getLang]
+    } else if (key === 'wikipediaCaption') {
       const pref = window.SKOSMOS.prefLabels.find(item => item.lang === getLang).label
       return {
-        "fi": pref + " Wikipediassa",
-        "sv": pref + " på Wikipedia",
-        "en": pref + " in Wikipedia"
-      }[getLang];
-    }
-    else if (key === "wikipediaTerms") {
+        fi: pref + ' Wikipediassa',
+        sv: pref + ' på Wikipedia',
+        en: pref + ' in Wikipedia'
+      }[getLang]
+    } else if (key === 'wikipediaTerms') {
       return {
-        "fi": 'Teksti on saatavilla <a rel="license" href="//fi.wikipedia.org/wiki/Wikipedia:Creative_Commons_Attribution-Share_Alike_3.0_Unported_-lisenssiehdot" target="_blank">Creative Commons Attribution/Share-Alike</a> -lisenssillä; lisäehtoja voi sisältyä. Katso <a href="//wikimediafoundation.org/wiki/Terms_of_Use/fi" target="_blank">käyttöehdot</a>. Wikipedia® on <a href="http://www.wikimediafoundation.org" target="_blank">Wikimedia Foundationin</a> rekisteröimä tavaramerkki.',
-        "sv": 'Wikipedias text är tillgänglig under licensen  <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.sv" target="_blank">Creative Commons Erkännande-dela-lika 3.0 Unported</a>. För bilder, se respektive bildsida (klicka på bilden). Se vidare <a href="//sv.wikipedia.org/wiki/Wikipedia:Upphovsrätt" target="_blank">Wikipedia:Upphovsrätt</a> och <a href="//wikimediafoundation.org/wiki/Terms_of_Use" target="_blank">användarvillkor</a>.',
-        "en": 'Text is available under the <a rel="license" href="//en.wikipedia.org/wiki/Wikipedia:Text_of_Creative_Commons_Attribution-ShareAlike_3.0_Unported_License" target="_blank">Creative Commons Attribution-ShareAlike License</a><a rel="license" href="//creativecommons.org/licenses/by-sa/3.0/" target="_blank" style="display:none;"></a>; additional terms may apply.  By using this site, you agree to the <a href="//wikimediafoundation.org/wiki/Terms_of_Use" target="_blank">Terms of Use</a> and <a href="//wikimediafoundation.org/wiki/Privacy_policy" target="_blank">Privacy Policy</a>. Wikipedia® is a registered trademark of the <a href="//www.wikimediafoundation.org/" target="_blank">Wikimedia Foundation, Inc.</a>, a non-profit organization.'
-      }[getLang];
-    }
-    else if (key === "wikipediaCredit") {
+        fi: 'Teksti on saatavilla <a rel="license" href="//fi.wikipedia.org/wiki/Wikipedia:Creative_Commons_Attribution-Share_Alike_3.0_Unported_-lisenssiehdot" target="_blank">Creative Commons Attribution/Share-Alike</a> -lisenssillä; lisäehtoja voi sisältyä. Katso <a href="//wikimediafoundation.org/wiki/Terms_of_Use/fi" target="_blank">käyttöehdot</a>. Wikipedia® on <a href="http://www.wikimediafoundation.org" target="_blank">Wikimedia Foundationin</a> rekisteröimä tavaramerkki.',
+        sv: 'Wikipedias text är tillgänglig under licensen  <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.sv" target="_blank">Creative Commons Erkännande-dela-lika 3.0 Unported</a>. För bilder, se respektive bildsida (klicka på bilden). Se vidare <a href="//sv.wikipedia.org/wiki/Wikipedia:Upphovsrätt" target="_blank">Wikipedia:Upphovsrätt</a> och <a href="//wikimediafoundation.org/wiki/Terms_of_Use" target="_blank">användarvillkor</a>.',
+        en: 'Text is available under the <a rel="license" href="//en.wikipedia.org/wiki/Wikipedia:Text_of_Creative_Commons_Attribution-ShareAlike_3.0_Unported_License" target="_blank">Creative Commons Attribution-ShareAlike License</a><a rel="license" href="//creativecommons.org/licenses/by-sa/3.0/" target="_blank" style="display:none;"></a>; additional terms may apply.  By using this site, you agree to the <a href="//wikimediafoundation.org/wiki/Terms_of_Use" target="_blank">Terms of Use</a> and <a href="//wikimediafoundation.org/wiki/Privacy_policy" target="_blank">Privacy Policy</a>. Wikipedia® is a registered trademark of the <a href="//www.wikimediafoundation.org/" target="_blank">Wikimedia Foundation, Inc.</a>, a non-profit organization.'
+      }[getLang]
+    } else if (key === 'wikipediaCredit') {
       return {
-        'fi': 'Katso sivu Wikipediassa: ',
-        'sv': 'Se sidan på Wikipedia: ',
-        'en': 'See the page in Wikipedia: '
-      }[getLang];
-    }
-    else {
-      return "";
+        fi: 'Katso sivu Wikipediassa: ',
+        sv: 'Se sidan på Wikipedia: ',
+        en: 'See the page in Wikipedia: '
+      }[getLang]
+    } else {
+      return ''
     }
   },
-  fixLinks: function (data, wikiLang) {
-    const temp = document.createElement('div');
-    temp.innerHTML = data;
+  fixLinks: function (data) {
+    const temp = document.createElement('div')
+    temp.innerHTML = data
 
-    const wikiAddress = 'https://' + wikiLang + '.wikipedia.org/wiki/';
+    const wikiAddress = 'https://' + WIKI.wikiLang + '.wikipedia.org/wiki/'
     const attrs = {
-      A:    ['href'],
+      A: ['href'],
       LINK: ['href'],
-      IMG:  ['src', 'srcset', 'resource']
-    };
+      IMG: ['src', 'srcset', 'resource']
+    }
 
-    const elements = temp.querySelectorAll('a, link, img');
+    const elements = temp.querySelectorAll('a, link, img')
     elements.forEach(elem => {
       if (elem.hash && elem.hash.startsWith('#cite_')) {
-        elem.href = WIKI.address + elem.hash;
+        elem.href = WIKI.address + elem.hash
       } else {
-        elem.target = '_blank';
+        elem.target = '_blank'
       }
 
-      const tagAttrs = attrs[elem.tagName];
-      if (!tagAttrs) return;
+      const tagAttrs = attrs[elem.tagName]
+      if (!tagAttrs) return
 
       tagAttrs.forEach(attr => {
-        WIKI.linkHelper(elem, attr, wikiAddress);
-      });
-    });
+        WIKI.linkHelper(elem, attr, wikiAddress)
+      })
+    })
 
-    const thumbinners = temp.querySelectorAll('.thumbinner');
+    const thumbinners = temp.querySelectorAll('.thumbinner')
     thumbinners.forEach(elem => {
-      const maxWidth = parseInt(elem.style.maxWidth, 10);
-      const width = parseInt(elem.style.width, 10);
+      const maxWidth = parseInt(elem.style.maxWidth, 10)
+      const width = parseInt(elem.style.width, 10)
 
       if (!isNaN(maxWidth)) {
-        elem.style.maxWidth = (maxWidth + 8) + 'px';
+        elem.style.maxWidth = (maxWidth + 8) + 'px'
       }
       if (!isNaN(width)) {
-        elem.style.width = (width + 8) + 'px';
+        elem.style.width = (width + 8) + 'px'
       }
-    });
+    })
 
-    return temp.innerHTML;
+    return temp.innerHTML
   },
   linkHelper: function (elem, attr, wikiAddress) {
     if (elem.hasAttribute(attr)) {
-      const value = elem.getAttribute(attr);
+      const value = elem.getAttribute(attr)
 
       if (value && value.startsWith('./')) {
         // fix relative links
-        elem.setAttribute(attr, wikiAddress + value.substring(2));
+        elem.setAttribute(attr, wikiAddress + value.substring(2))
       } else if (value && value.startsWith('//')) {
         // force https
-        elem.setAttribute(attr, 'https:' + value);
+        elem.setAttribute(attr, 'https:' + value)
       } else if (value && value.startsWith('/wiki/')) {
-        elem.setAttribute(attr, wikiAddress + value.substring(6));
+        elem.setAttribute(attr, wikiAddress + value.substring(6))
       }
     }
   },
-  generateQueryString: function (wikiLang, url) {
-      var title = url.substring(url.lastIndexOf('/') + 1, url.length);
-      return 'https://' + wikiLang +'.wikipedia.org/api/rest_v1/page/html/' + title;
+  generateQueryString: function (url) {
+    const title = url.substring(url.lastIndexOf('/') + 1, url.length)
+    return 'https://' + WIKI.wikiLang + '.wikipedia.org/api/rest_v1/page/html/' + title
   },
-  //generateTOC: function () {}, //TODO?
+  // generateTOC: function () {}, //TODO?
   updateAddress: function () {
-      this.address = window.location.protocol + "//" +  window.location.host + window.location.pathname + window.location.search;
+    this.address = window.location.protocol + '//' + window.location.host + window.location.pathname + window.location.search
   },
   updateWikipediaURL: function (url) {
-      this.wikipediaURL = url;
+    this.wikipediaURL = url
   },
-  queryWiki: function(url, wikiLang) {
+  queryWikidata: function (wikidataId) {
     const headers = new Headers({
-      "Accept": "text/html; charset=utf-8; profile='https://www.mediawiki.org/wiki/Specs/HTML/1.6.0'",
-      "Api-User-Agent": "Finto.fi wikipedia widget - finto-posti@helsinki.fi"
+      'Api-User-Agent': 'Finto.fi wikipedia widget - finto-posti@helsinki.fi'
     })
-
+    let url = 'https://www.wikidata.org/w/api.php?action=wbgetentities&props=sitelinks&origin=*&format=json&sitefilter='
+    const languageCodes = ['fi', 'sv', 'en', 'se']
+    for (const idx in languageCodes) {
+      url += languageCodes[idx] + 'wiki|'
+    }
+    url = url.slice(0, -1)
+    url += '&ids=' + wikidataId
+    let wikiLabel = null
+    return fetch(url, { headers })
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        const siteLinks = data.entities[wikidataId].sitelinks
+        if (WIKI.wikiLang + 'wiki' in siteLinks) {
+          wikiLabel = siteLinks[WIKI.wikiLang + 'wiki'].title
+        } else {
+          for (const idx in languageCodes) {
+            const siteName = languageCodes[idx] + 'wiki'
+            if (siteName in siteLinks) {
+              wikiLabel = siteLinks[siteName].title
+            }
+          }
+        }
+        return wikiLabel
+      })
+  },
+  queryWikipedia: function (url) {
+    const headers = new Headers({
+      Accept: "text/html; charset=utf-8; profile='https://www.mediawiki.org/wiki/Specs/HTML/1.6.0'",
+      'Api-User-Agent': 'Finto.fi wikipedia widget - finto-posti@helsinki.fi'
+    })
     fetch(url, { headers })
       .then(response => {
         return response.text()
@@ -183,26 +208,24 @@ const WIKI = {
       .then(data => {
         WIKI.succeeded = true
         // clean data for rendering purposes
-        //take only sections
-        const n = data.indexOf("<section");
-        const m = data.lastIndexOf("</section>") + 10;
-        let cleaned = data.substring(n, m);
+        // take only sections
+        const n = data.indexOf('<section')
+        const m = data.lastIndexOf('</section>') + 10
+        let cleaned = data.substring(n, m)
         // fix links in json data
-        cleaned = cleaned.replace(/href":"\.\//g, "https://" + wikiLang + ".wikipedia.org/wiki/");
+        cleaned = cleaned.replace(/href":"\.\//g, 'https://' + WIKI.wikiLang + '.wikipedia.org/wiki/')
         // fix too eager downloading of image sources
-        cleaned = cleaned.replace(/src="\/(?!\/)/g, 'src="https://' + wikiLang + ".wikipedia.org/");
+        cleaned = cleaned.replace(/src="\/(?!\/)/g, 'src="https://' + WIKI.wikiLang + '.wikipedia.org/')
 
         // fix links in dom nodes
-        cleaned = WIKI.fixLinks(cleaned, wikiLang);
+        cleaned = WIKI.fixLinks(cleaned, WIKI.wikiLang)
         WIKI.wikipediaHTML = cleaned
         this.render()
       })
       .catch(error => {
         WIKI.succeeded = false
-        this.render({
-          succeeded: false,
-          message: WIKI.getTranslation("error")
-        })
+        WIKI.message = WIKI.getTranslation('error')
+        this.render()
       })
   },
   render: function (object) {
@@ -219,7 +242,6 @@ const WIKI = {
 
     this.vueApp = this.createVueApp()
     this.vueApp.mount('#wiki-plugin')
-
   },
   remove: function () {
     if (this.vueApp) {
@@ -230,7 +252,6 @@ const WIKI = {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-
   window.wikiWidget = function (data) {
     // Only activate the widget when
     // 1) on a concept page
@@ -238,38 +259,54 @@ document.addEventListener('DOMContentLoaded', function () {
     // 3) and the json-ld data can be found
     // 4) and there exists a wikidata object
     if (data.pageType !== 'concept' || data.prefLabels === undefined || Object.keys(data.jsonLd).length === 0) {
-        return;
+      return
     }
-    var wikidata;
+
+    WIKI.wikiLang = window.SKOSMOS.content_lang
+    const context = data.jsonLd['@context']
+    const jsonLdUriSpace = Object.keys(context).find(key => context[key] === window.SKOSMOS.uriSpace)
+    const skosmosUriSpace = window.SKOSMOS.uriSpace
+    const jsonLdUri = data.uri.replace(skosmosUriSpace, jsonLdUriSpace + ':')
     const graph = data.jsonLd.graph
 
-    for (const value of graph) {
-      if (value.uri.startsWith("wd:")) {
-        wikidata = value.uri
-        break
+    const closeMatches = []
+    for (const concept of graph) {
+      if (concept.uri === jsonLdUri) {
+        if (Array.isArray(concept['skos:closeMatch'])) {
+          for (const cm of concept['skos:closeMatch']) {
+            if (cm.uri.startsWith('wd:')) {
+              const wikidataId = cm.uri.replace('wd:', '')
+              closeMatches.push(wikidataId)
+            }
+            // if Skosmos fails to fetch Wikidata:
+            else if (cm.uri.startsWith('http://www.wikidata.org/wiki/')) {
+              const wikidataId = cm.uri.replace('http://www.wikidata.org/wiki/', '')
+              closeMatches.push(wikidataId)
+            }
+          }
+        } else {
+          if (concept['skos:closeMatch'].uri.startsWith('wd:')) {
+            const wikidataId = concept['skos:closeMatch'].uri.replace('wd:', '')
+            closeMatches.push(wikidataId)
+          }
+        }
       }
     }
-
-    if (!wikidata) {
-        return;
+    if (!closeMatches.length) {
+      return
     }
-
-    var wikiURL = null;
-    WIKI.wikiLang;
-    const prefLabel = window.SKOSMOS.prefLabels.find(item => item.lang === window.SKOSMOS.lang).label
-
-    // quick fix for missing data containing Wikipedia URLs:
-    const pref = window.SKOSMOS.prefLabels.find(item => item.lang === window.SKOSMOS.lang).label
-    WIKI.restURL = WIKI.generateQueryString(window.SKOSMOS.lang, "https://fi.wikipedia.org/wiki/" + prefLabel);
-
-    if (WIKI.restURL) {
-      WIKI.updateWikipediaURL(WIKI.restURL);
-      WIKI.updateAddress();
-      WIKI.queryWiki(WIKI.restURL, WIKI.wikiLang);
-    }
-    else {
-      WIKI.widget.render({succeeded: false, message: WIKI.getTranslation("404")});
-    }
+    WIKI.queryWikidata(closeMatches[0])
+      .then(wikiLabel => {
+        if (wikiLabel) {
+          const restURL = WIKI.generateQueryString('https://fi.wikipedia.org/wiki/' + wikiLabel)
+          WIKI.updateWikipediaURL(restURL)
+          WIKI.updateAddress()
+          WIKI.queryWikipedia(restURL)
+        } else {
+          WIKI.succeeded = false
+          WIKI.message = WIKI.getTranslation('404')
+          WIKI.render()
+        }
+      })
   }
-
 })
